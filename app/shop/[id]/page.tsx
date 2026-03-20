54b70a2532ad88edeeea923757e69d963c64732b
developmentrankmantra-sudo <developmentrankmantra@gmail.com>
2026-03-20 11:57:59 +0530

    fix: add generateStaticParams for dynamic product pages

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -1,4 +1,11 @@
 import ProductClient from "./ProductClient";
+import products from "@/lib/products.json";
+
+export async function generateStaticParams() {
+  return products.map((product) => ({
+    id: product.slug || product.id,
+  }));
+}
 
 export default function ProductPage() {
   return <ProductClient />;
